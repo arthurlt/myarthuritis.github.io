aafb6e10e1e78dff42433d5d7b2ea70579a23068
Arthur <arthurlt@gmail.com>
2012-07-08 01:29:37 -0600

    added sharing buttons + centered time better

---
+++ script.js
@@ -0,0 +1,45 @@
+<script type="text/javascript">
+function startTime()
+    {
+    var today = new Date();
+    var hour = today.getHours();
+    var minute = today.getMinutes();
+    var date = today.getDate();
+    var day = today.getDay();
+    var thismonth = today.getMonth();
+    var weekday = new Array(7);
+        weekday[0]="Sun";
+        weekday[1]="Mon";
+        weekday[2]="Tue";
+        weekday[3]="Wed";
+        weekday[4]="Thu";
+        weekday[5]="Fri";
+        weekday[6]="Sat";
+    var month = new Array(13)
+        month[0]="Jan";
+        month[1]="Feb";
+        month[2]="Mar";
+        month[3]="Apr";
+        month[4]="May";
+        month[5]="Jun";
+        month[6]="Jul";
+        month[7]="Aug";
+        month[8]="Sep";
+        month[9]="Oct";
+        month[10]="Nov";
+        month[11]="Dec";
+        month[12]="test";
+    minute = checkTime(minute);
+    document.getElementById('time').innerHTML=(weekday[day])+" "+(month[thismonth])+"  "+date+" "+hour+":"+minute;
+    t = setTimeout('startTime()',1000);
+    }
+
+function checkTime(i)
+    {
+    if (i<10)
+      {
+      i="0" + i;
+      }
+    return i;
+    }
+</script>
