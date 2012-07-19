55b2474991e442db1bc63ba4e19411ef34aae782
Mars11 <arthurlt@gmail.com>
2012-07-19 01:23:55 -0600

    added draggable windows

---
+++ js/img.js
@@ -0,0 +1,4 @@
+function mouseOverImage() {
+        document.getElementById("maximize").style.background = "url(img/window/unmaximize-hover.svg)";}
+function mouseOutImage() { 
+        document.getElementById("maximize").style.background = "url(img/window/unmaximize-normal.svg)";}
