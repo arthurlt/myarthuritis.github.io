e68f3c716dc0e29323e8c2a9c154742ae9449be0
Mars11 <arthurlt@gmail.com>
2012-07-19 16:00:05 -0600

    added mobile site

--- js/showhide.js
+++ js/showhide.js
@@ -2,25 +2,26 @@ function toggle_visibility(id) {
     var e = document.getElementById(id);
     var a = document.getElementById("aboutwindow")
     var d = document.getElementById("devwindow")
-    var g = document.getElementById("googlewindow")
-    var t = document.getElementById("twitterwindow")
+    //var g = document.getElementById("googlewindow")
+    //var t = document.getElementById("twitterwindow")
 //closing other window if open or not
     if (e == a) {
-        d.style.display = "none";
-        g.style.display = "none";
-        t.style.display = "none";}
+        d.style.display = "none";}
+        //g.style.display = "none";
+        //t.style.display = "none";}
     else if (e == d) {
-        a.style.display = "none";
-        g.style.display = "none";
-        t.style.display = "none";}
-    else if (e == g) {
-        a.style.display = "none";
-        d.style.display = "none";
-        t.style.display = "none";}
-    else {
-        a.style.display = "none";
-        d.style.display = "none";
-        g.style.display = "none";}
+        a.style.display = "none";}
+        //g.style.display = "none";
+        //t.style.display = "none";}
+    else { //if (e == g) {
+        //a.style.display = "none";
+        //d.style.display = "none";
+        //t.style.display = "none";
+          e.style.display = "none";}
+   // else {
+        //a.style.display = "none";
+        //d.style.display = "none";
+        //g.style.display = "none";}
 //toggling window open
     if (e.style.display == 'block') {
         e.style.display = 'none';}
