ace3b9e6400e9507fc404480138f4f731da703b7
Mars11 <arthurlt@gmail.com>
2012-07-19 11:46:59 -0600

    added iframe browser

--- js/showhide.js
+++ js/showhide.js
@@ -2,13 +2,25 @@ function toggle_visibility(id) {
     var e = document.getElementById(id);
     var a = document.getElementById("aboutwindow")
     var d = document.getElementById("devwindow")
+    var g = document.getElementById("googlewindow")
+    var t = document.getElementById("twitterwindow")
 //closing other window if open or not
     if (e == a) {
-        d.style.display = "none";}
+        d.style.display = "none";
+        g.style.display = "none";
+        t.style.display = "none";}
     else if (e == d) {
-        a.style.display = "none";}
+        a.style.display = "none";
+        g.style.display = "none";
+        t.style.display = "none";}
+    else if (e == g) {
+        a.style.display = "none";
+        d.style.display = "none";
+        t.style.display = "none";}
     else {
-        e.style.display = "none";}
+        a.style.display = "none";
+        d.style.display = "none";
+        g.style.display = "none";}
 //toggling window open
     if (e.style.display == 'block') {
         e.style.display = 'none';}
