a4fc8b1f7e0659b2007c1da94a143d23c14919d4
Mars11 <arthurlt@gmail.com>
2012-07-19 01:30:49 -0600

    hopefully fixed redirect

--- js/url.js
+++ js/url.js
@@ -1,2 +1,6 @@
-//var stateObj = { index: "dev" };
-//history.pushState(stateObj, "page 2", "dev.html");
+function devurl() {
+    var stateObj = { index: "dev" };
+    history.pushState(stateObj, "page 2", "dev.html");}
+function abouturl() {
+    var stateObj = { index: "about" };
+    history.pushState(stateObj, "page 2", "about.html");}
