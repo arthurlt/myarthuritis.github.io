b6c60276620d15ebb10182f9c2dffaf0b6729f57
Mars11 <arthurlt@gmail.com>
2012-10-18 13:24:22 -0600

    reworking site,no windows, no dock, no wallpaper

--- js/img.js
+++
@@ -1,4 +0,0 @@
-function mouseOverImage() {
-		document.getElementById("maximize").style.background = "url(img/window/unmaximize-hover.svg)";}
-function mouseOutImage() { 
-		document.getElementById("maximize").style.background = "url(img/window/unmaximize-normal.svg)";}
--- js/url.js
+++
@@ -1,9 +0,0 @@
-function mainurl() {
-	var stateObj = { url: "#" };
-	history.pushState(stateObj, "page 1", "#");}
-function devurl() {
-	var stateObj = { index: "#dev" };
-	history.pushState(stateObj, "page 2", "#dev");}
-function abouturl() {
-	var stateObj = { foo: "#about" };
-	history.pushState(stateObj, "page 3", "#about");}
