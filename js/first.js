a649932c147dbc282983c5dbc3c96c14705256a6
Mars11 <arthurlt@gmail.com>
2012-10-20 01:13:52 -0600

    moved all seperate js functions into either first, or third part js files

--- js/first.js
+++ js/first.js
@@ -49,3 +49,25 @@ function checkHour(h) {
     }
   return h;
   }
+
+//function startEE(hour,minute) {
+//  setTimeout('hourChime(hour,minute);',60000);
+//  }
+var chimes = 0
+function hourChime(hour,minute) {
+  if (hour==00)
+  {
+  if (minute==00)
+    {
+    if (chimes<6)
+    {
+    document.getElementById('chime').play();
+    }
+    }
+    else
+    {
+    chimes = 0
+    }
+  }
+  document.getElementById("jsdebug").innerHTML="chimed "+chimes+" times";
+  }
