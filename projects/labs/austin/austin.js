a99c13ad8301e44810eaf3d7a033dc83365f30ae
Arthur Tucker <arthurlt@gmail.com>
2013-02-24 22:25:48 -0700

    Updating .html pages for new js.

--- projects/labs/austin/austin.js
+++ projects/labs/austin/austin.js
@@ -2,7 +2,7 @@ function equal() {
 	var num1=prompt("Enter a number...");
 	var num2=prompt("Enter another number...");
 	if (num1===num2) {
-		showNotify("Those numbers are equal!");
+		showNotify("Those numbers are equal!","rgba(0, 230, 0, 0.95)");
 	} else {
 		showNotify("Those numbers arn't equal!");
 	};
